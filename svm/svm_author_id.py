c7dc1854cc0cc5b127123df32b12bf2cb7a3c81b
Haya Aldossary <haya.e.dossary@gmail.com>
2017-06-22 02:32:20 +0300

    Add files via upload

---
+++ svm/svm_author_id.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+
+#########################################################
+
+t0 = time()
+clf = SVC(kernel = "rbf", C = 10000) 
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+acc = accuracy_score(pred, labels_test)
+print "The predcition accuracy is: ",acc
+print "prediction for the 10th test case",pred[10]
+print "prediction for the 26th test case",pred[26]
+print "prediction for the 50th test case",pred[50]
+s = 0
+
+for p in pred:
+	if p == 1:
+		s += 1
+print s
